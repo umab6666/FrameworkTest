26bef6adfcbac27e2009d942edc52d2c153733d6
uma@thisisswitch.in <uma@thisisswitch.in>
2019-02-18 12:31:19 +0530

    Initial Commit

---
+++ FrameworkTest/FrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTest.h
+//  FrameworkTest
+//
+//  Created by Uma B on 18/02/19.
+//  Copyright © 2019 switchSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkTest.
+FOUNDATION_EXPORT double FrameworkTestVersionNumber;
+
+//! Project version string for FrameworkTest.
+FOUNDATION_EXPORT const unsigned char FrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest/PublicHeader.h>
+
+
